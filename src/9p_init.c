13a99023958161275da1036a04ee0aa6c5a0a1ae
Dominique Martinet <dominique.martinet@cea.fr>
2013-07-29 14:41:29 +0200

    default port different for tcp and rdma

--- src/9p_init.c
+++ src/9p_init.c
@@ -121,6 +121,7 @@ int parser(char *conf_file, struct p9_conf *p9_conf) {
 	struct hostent *host;
 	char buf_s[MAXNAMLEN];
 	int buf_i;
+	uint16_t port;
 	void *ptr;
 	rc = 0;
 
@@ -134,6 +135,7 @@ int parser(char *conf_file, struct p9_conf *p9_conf) {
 
 	// fill default values.
 	memset(p9_conf, 0, sizeof(struct p9_conf));
+	port = 0;
 	p9_conf->trans_attr.server = -1;
 	p9_conf->trans_attr.disconnect_callback = p9_disconnect_cb;
 	p9_conf->trans_attr.max_send_sge = 2;
@@ -211,9 +213,6 @@ int parser(char *conf_file, struct p9_conf *p9_conf) {
 					// Sanity check: we got an IP
 					p9_conf->trans_attr.server = 0;
 
-					// Default port. depends on the sin family
-					((struct sockaddr_in*) &p9_conf->trans_attr.addr)->sin_port = htons(DEFAULT_PORT);
-
 					INFO_LOG(p9_conf->debug & P9_DEBUG_SETUP, "Read %s: %s", conf_array[i].token, buf_s);
 					break;
 				case PORT:
@@ -222,7 +221,9 @@ int parser(char *conf_file, struct p9_conf *p9_conf) {
 						rc = EINVAL;
 						goto out;
 					}
-					((struct sockaddr_in*) &p9_conf->trans_attr.addr)->sin_port = htons(buf_i);
+
+					port = buf_i;
+
 					INFO_LOG(p9_conf->debug & P9_DEBUG_SETUP, "Read %s: %i", conf_array[i].token, buf_i);
 					break;
 				case NET_TYPE:
@@ -251,6 +252,20 @@ int parser(char *conf_file, struct p9_conf *p9_conf) {
 		if (conf_array[i].token == NULL) {
 			ERROR_LOG("Unknown configuration entry: %s", line);
 		}
+	}
+
+	// Default port. depends on the sin family
+	if (port == 0) {
+		if (p9_conf->net_ops == &p9_tcp_ops)
+			((struct sockaddr_in*) &p9_conf->trans_attr.addr)->sin_port = htons(DEFAULT_PORT_TCP);
+#if HAVE_MOOSHIKA
+		else if(p9_conf->net_ops == &p9_rdma_ops)
+			((struct sockaddr_in*) &p9_conf->trans_attr.addr)->sin_port = htons(DEFAULT_PORT_RDMA);
+#endif
+		else
+			ERROR_LOG("ops neither tcp nor rdma?");
+	} else {
+		((struct sockaddr_in*) &p9_conf->trans_attr.addr)->sin_port = htons(port);
 	}
 
 out:
